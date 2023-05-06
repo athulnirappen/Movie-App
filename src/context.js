549699c1e2c9716293a5dd80a73d6b198d560c67
Athul Thomas <athulthomas001798@gmail.com>
2023-05-06 10:47:55 +0530

    last commit

--- src/context.js
+++ src/context.js
@@ -7,55 +7,55 @@ import { createContext, useContext, useEffect, useState } from "react";
  export const MovieContext = createContext();
 
 
-export const MovieContextProvider = ({children}) => {
+export const MovieContextProvider = ({ children }) => {
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [movie, setMovie] = useState([]);
-    const [isError, setIsError] = useState({ show: "false", msg: "" });
-    const [query,setQuery]=useState('titanic')
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const [isError, setIsError] = useState({ show: "false", msg: "" });
+  const [query, setQuery] = useState('titanic')
 
-    const getMovies =async (url) => {
-        try {
+  const getMovies = async (url) => {
+    try {
 
-            const res = await fetch(url);
-            const data =await res.json();
-            console.log(data);
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
 
-            if (data.Response === "True") {
-                setIsLoading(false)
-                setIsError({
-                  show: false,
-                  msg: "",
-                });
-                setMovie(data.Search)
-            } else {
-                setIsError({
-                    show: true,
-                    msg:data.Error
-                })
-            }
+      if (data.Response === "True") {
+        setIsLoading(false)
+        setIsError({
+          show: false,
+          msg: "",
+        });
+        setMovie(data.Search)
+      } else {
+        setIsError({
+          show: true,
+          msg: data.Error
+        })
+      }
             
-        } catch (error) {
-            console.log(error);
-        }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    useEffect(() => {
-      let timeOut= setTimeout(() => {
-            getMovies(`${API_URL}&s=${query}`); 
-        }, 800)
+  useEffect(() => {
+    let timeOut = setTimeout(() => {
+      getMovies(`${API_URL}&s=${query}`);
+    }, 800)
 
-        return ()=>clearTimeout(timeOut)
+    return () => clearTimeout(timeOut)
        
-    },[query])
-    return (
-      <MovieContext.Provider
-        value={{ isLoading, isError, movie, query, setQuery }}
-      >
-        {children}
-      </MovieContext.Provider>
-    );
- } 
+  }, [query])
+  return (
+    <MovieContext.Provider
+      value={{ isLoading, isError, movie, query, setQuery }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+}
 
 
 export const useMovieContext = () => {
